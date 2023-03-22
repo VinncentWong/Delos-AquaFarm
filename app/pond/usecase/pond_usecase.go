b1d7afe9dd5c8aa1a9290ff9a5e5a04fafadf982
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-22 16:29:34 +0700

    [ADD] create pond usecase

---
+++ app/pond/usecase/pond_usecase.go
@@ -0,0 +1,41 @@
+package usecase
+
+import (
+	"errors"
+
+	farmRepository "github.com/VinncentWong/Delos-AquaFarm/app/farm/repository"
+	pondRepository "github.com/VinncentWong/Delos-AquaFarm/app/pond/repository"
+	"github.com/VinncentWong/Delos-AquaFarm/domain"
+)
+
+type IPondUsecase interface {
+	CreatePond(id string, pond *domain.Pond) (domain.Pond, error)
+}
+
+type PondUsecase struct {
+	pondRepository pondRepository.IPondRepository
+	farmRepository farmRepository.IFarmRepository
+}
+
+func NewPondUsecase(pondRepository pondRepository.IPondRepository, farmRepository farmRepository.IFarmRepository) IPondUsecase {
+	return &PondUsecase{
+		pondRepository: pondRepository,
+		farmRepository: farmRepository,
+	}
+}
+
+func (u *PondUsecase) CreatePond(idFarm string, pond *domain.Pond) (domain.Pond, error) {
+	_, err := u.farmRepository.GetFarmById(idFarm)
+	if err != nil {
+		return domain.Pond{}, errors.New("farm id doesn't exist")
+	}
+	_, err = u.pondRepository.GetPondByName(pond.Name)
+	if err == nil {
+		return domain.Pond{}, errors.New("pond already exist in database, duplicate data was denied")
+	}
+	result, err := u.pondRepository.CreatePond(idFarm, pond)
+	if err != nil {
+		return domain.Pond{}, err
+	}
+	return result, nil
+}
