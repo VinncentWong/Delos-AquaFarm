5d8adf940f25d1a6e919d0776b31551f999debcb
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-22 16:29:50 +0700

    [ADD] get farm by id

--- app/farm/repository/farm_repository.go
+++ app/farm/repository/farm_repository.go
@@ -1,9 +1,6 @@
 package repository
 
 import (
-	"errors"
-	"fmt"
-
 	"github.com/VinncentWong/Delos-AquaFarm/domain"
 	"github.com/VinncentWong/Delos-AquaFarm/infrastructure"
 	"gorm.io/gorm"
@@ -12,6 +9,7 @@ import (
 type IFarmRepository interface {
 	CreateFarm(farm *domain.Farm) (domain.Farm, error)
 	GetFarmByName(name string) (domain.Farm, error)
+	GetFarmById(id string) (domain.Farm, error)
 }
 
 type FarmRepository struct {
@@ -25,15 +23,7 @@ func NewFarmRepository() IFarmRepository {
 }
 
 func (r *FarmRepository) CreateFarm(data *domain.Farm) (domain.Farm, error) {
-	fmt.Println("data.Farmname = ", data.FarmName)
-	farm, err := r.GetFarmByName(data.FarmName)
-	if err != nil {
-		return domain.Farm{}, nil
-	}
-	if len(farm.FarmName) != 0 {
-		return domain.Farm{}, errors.New("farm already exist in database, a duplicate should be denied")
-	}
-	err = r.db.Save(data).Error
+	err := r.db.Save(data).Error
 	if err != nil {
 		return domain.Farm{}, err
 	}
@@ -42,9 +32,18 @@ func (r *FarmRepository) CreateFarm(data *domain.Farm) (domain.Farm, error) {
 
 func (r *FarmRepository) GetFarmByName(name string) (domain.Farm, error) {
 	var container domain.Farm
-	result := r.db.Where("farm_name = ?", name).Take(&container)
+	result := r.db.Where("farm_name = ?", name).Take(&container).Limit(1)
 	if result.Error != nil {
-		return domain.Farm{}, nil
+		return domain.Farm{}, result.Error
 	}
 	return container, nil
+}
+
+func (r *FarmRepository) GetFarmById(id string) (domain.Farm, error) {
+	var container domain.Farm
+	result := r.db.Where("id = ?", id).Take(&container)
+	if result.Error != nil {
+		return domain.Farm{}, result.Error
+	}
+	return container, result.Error
 }
