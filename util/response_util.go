e3089308759c4a842e829eea60ea963f0a97bf98
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-22 14:30:48 +0700

    [REFACTOR] typo on response success key

--- util/response_util.go
+++ util/response_util.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func SendResponse(c *gin.Context, code int, message string, success bool, data any) {
 	c.JSON(code, gin.H{
-		"succes":  success,
+		"success": success,
 		"message": message,
 		"data":    data,
 	})
