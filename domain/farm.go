041687ffb27cbe86c3c86e53fa57312c8c1baeda
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-22 16:53:25 +0700

    [ADD] validator

--- domain/farm.go
+++ domain/farm.go
@@ -7,6 +7,6 @@ This is a domain for Farm
 */
 type Farm struct {
 	gorm.Model
-	FarmName string `json:"name" binding:"required" gorm:"type:varchar(50)"`
-	Ponds    []Pond `gorm:"foreignKey:FarmID;constraint:OnDelete:CASCADE" json:"-"`
+	FarmName string `json:"name" gorm:"type:varchar(50);not null" validate:"gte=4"`
+	Ponds    []Pond `gorm:"foreignKey:FarmID;constraint:OnDelete:CASCADE" json:"-" validate:"-"`
 }
