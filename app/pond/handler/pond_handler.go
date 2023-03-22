49d074bf1ef9885a563d0142dda79c3302f532cc
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-22 19:05:18 +0700

    [ADD] update pond

--- app/pond/handler/pond_handler.go
+++ app/pond/handler/pond_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/VinncentWong/Delos-AquaFarm/app/pond/usecase"
 	"github.com/VinncentWong/Delos-AquaFarm/domain"
@@ -36,8 +37,41 @@ func (h *PondHandler) CreatePond(c *gin.Context) {
 	}
 	result, err := h.usecase.CreatePond(farmId, &container)
 	if err != nil {
-		util.SendResponse(c, http.StatusBadRequest, err.Error(), false, nil)
+		switch err.Error() {
+		case "farm id doesn't exist":
+			util.SendResponse(c, http.StatusNotFound, err.Error(), false, nil)
+		default:
+			util.SendResponse(c, http.StatusInternalServerError, err.Error(), false, nil)
+		}
 		return
 	}
 	util.SendResponse(c, http.StatusCreated, "success create pond", true, result)
+}
+
+func (h *PondHandler) UpdatePond(c *gin.Context) {
+	pondId := c.Param("pondId")
+	var container domain.Pond
+	err := c.ShouldBindJSON(&container)
+	if err != nil {
+		util.SendResponse(c, http.StatusBadRequest, err.Error(), false, nil)
+		return
+	}
+	err = validator.New().Struct(&container)
+	if err != nil {
+		errs := err.(validator.ValidationErrors)
+		util.SendResponse(c, http.StatusBadRequest, errs.Error(), false, nil)
+		return
+	}
+	pondIdInt, err := strconv.Atoi(pondId)
+	if err != nil {
+		util.SendResponse(c, http.StatusBadRequest, err.Error(), false, nil)
+		return
+	}
+	container.ID = uint(pondIdInt)
+	err = h.usecase.UpdatePond(&container)
+	if err != nil {
+		util.SendResponse(c, http.StatusInternalServerError, err.Error(), false, nil)
+		return
+	}
+	util.SendResponse(c, http.StatusOK, "success update pond", true, nil)
 }
