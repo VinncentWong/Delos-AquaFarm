7a2998a9fcdc7ced5c980514126661522a4b62b0
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-23 14:35:46 +0700

    [ADD] method name field

--- domain/record.go
+++ domain/record.go
@@ -7,7 +7,8 @@ This is a domain for RecordApi domain
 */
 type RecordApi struct {
 	gorm.Model
-	Endpoint  string `json:"endpoint" gorm:"type:varchar(100)"`
-	IpAddress string `json:"ip_address" gorm:"type:varchar(255)"`
-	Count     uint   `json:"count"`
+	Endpoint   string `json:"endpoint" gorm:"type:varchar(100)"`
+	IpAddress  string `json:"ip_address" gorm:"type:varchar(255)"`
+	Count      uint   `json:"count"`
+	MethodName string `json:"method_name"`
 }
