6a8f9a86dfb825fb81b6e4f7026eff0a81d24242
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-23 14:36:29 +0700

    [ADD] record routing

--- rest/api.go
+++ rest/api.go
@@ -5,6 +5,7 @@ import (
 
 	farmHandler "github.com/VinncentWong/Delos-AquaFarm/app/farm/handler"
 	pondHandler "github.com/VinncentWong/Delos-AquaFarm/app/pond/handler"
+	recordHandler "github.com/VinncentWong/Delos-AquaFarm/app/record/handler"
 	"github.com/gin-gonic/gin"
 )
 
@@ -32,6 +33,7 @@ func (r *Routing) InitializeFarmRouting(handler *farmHandler.FarmHandler) {
 	farmGroup.PUT("/update/:farmId", handler.UpdateFarm)
 	farmGroup.DELETE("/delete/:farmId", handler.DeleteFarm)
 	farmGroup.GET("/gets", handler.GetAll)
+	farmGroup.GET("/get/:farmId", handler.GetFarmById)
 }
 
 func (r *Routing) InitializePondRouting(handler *pondHandler.PondHandler) {
@@ -40,4 +42,10 @@ func (r *Routing) InitializePondRouting(handler *pondHandler.PondHandler) {
 	pondGroup.PUT("/update/:pondId", handler.UpdatePond)
 	pondGroup.DELETE("/delete/:pondId", handler.DeletePond)
 	pondGroup.GET("/gets", handler.GetAll)
+	pondGroup.GET("/get/:pondId", handler.GetPondById)
+}
+
+func (r *Routing) InitializeRecordRouting(handler *recordHandler.RecordHandler) {
+	recordGroup := r.router.Group("/record")
+	recordGroup.GET("/gets", handler.GetAll)
 }
