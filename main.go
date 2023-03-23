b64a5dbef468e82d796f01fe99c0c948f9a90a2d
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-23 14:37:40 +0700

    [ADD] record middleware and init record handler,usecase, and repo

--- main.go
+++ main.go
@@ -11,7 +11,12 @@ import (
 	pondHandler "github.com/VinncentWong/Delos-AquaFarm/app/pond/handler"
 	pondRepository "github.com/VinncentWong/Delos-AquaFarm/app/pond/repository"
 	pondUsecase "github.com/VinncentWong/Delos-AquaFarm/app/pond/usecase"
+	recordHandler "github.com/VinncentWong/Delos-AquaFarm/app/record/handler"
+	recordRepository "github.com/VinncentWong/Delos-AquaFarm/app/record/repository"
+	recordUsecase "github.com/VinncentWong/Delos-AquaFarm/app/record/usecase"
 	"github.com/VinncentWong/Delos-AquaFarm/infrastructure"
+	"github.com/VinncentWong/Delos-AquaFarm/middleware/filter"
+	"github.com/VinncentWong/Delos-AquaFarm/middleware/provider"
 	"github.com/VinncentWong/Delos-AquaFarm/rest"
 	"github.com/gin-gonic/gin"
 )
@@ -43,6 +48,11 @@ func main() {
 	// init router
 	router := gin.Default()
 
+	// Init Middleware
+	recordProvider := provider.NewRecordProvider()
+	recordMiddleware := filter.NewRecordMiddleware(recordProvider)
+	router.Use(recordMiddleware.SaveTrackedData())
+
 	// init farm
 	fRepo := farmRepository.NewFarmRepository()
 	fUsecase := farmUsecase.NewFarmUsecase(fRepo)
@@ -53,11 +63,17 @@ func main() {
 	pUsecase := pondUsecase.NewPondUsecase(pRepo, fRepo)
 	pHandler := pondHandler.NewPondHandler(pUsecase)
 
+	// init record
+	rRepo := recordRepository.NewRecordRepository()
+	rUsecase := recordUsecase.NewRecordUsecase(rRepo)
+	rHandler := recordHandler.NewRecordHandler(rUsecase)
+
 	// init routing on rest directory
 	routing := rest.NewRouting(router)
 	routing.InitializeCheckHealthRouting()
 	routing.InitializeFarmRouting(fHandler)
 	routing.InitializePondRouting(pHandler)
+	routing.InitializeRecordRouting(rHandler)
 
 	router.Run(fmt.Sprintf(":%s", os.Getenv("APP_PORT")))
 }
