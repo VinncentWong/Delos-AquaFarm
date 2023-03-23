9d0ba7ea1b5f3afde9c1898c0e5877ccb5bb79bf
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-03-23 14:35:57 +0700

    [ADD] record middleware

---
+++ middleware/filter/record_middleware.go
@@ -0,0 +1,40 @@
+package filter
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/VinncentWong/Delos-AquaFarm/domain"
+	"github.com/VinncentWong/Delos-AquaFarm/middleware/provider"
+	"github.com/VinncentWong/Delos-AquaFarm/util"
+	"github.com/gin-gonic/gin"
+)
+
+type RecordMiddleware struct {
+	provider provider.IRecordProvider
+}
+
+func NewRecordMiddleware(provider provider.IRecordProvider) *RecordMiddleware {
+	return &RecordMiddleware{
+		provider: provider,
+	}
+}
+
+func (r *RecordMiddleware) SaveTrackedData() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ipAddress := util.GetIpAddress(c)
+		fmt.Println("ipAddress = ", ipAddress)
+		record := domain.RecordApi{
+			Endpoint:   c.Request.URL.Path,
+			IpAddress:  ipAddress,
+			MethodName: c.Request.Method,
+		}
+		err := r.provider.SaveRecord(&record)
+		if err != nil {
+			c.Abort()
+			util.SendResponse(c, http.StatusInternalServerError, err.Error(), false, nil)
+			return
+		}
+		c.Next()
+	}
+}
